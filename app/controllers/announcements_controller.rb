3b55beac03c053610fa2f8d170db016b1169129a
Deshmukh-Snehal <deshmukhs344@gmail.com>
2022-02-22 18:05:10 +0530

    add admin create,update,delete access conditions for announcement module

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -12,21 +12,21 @@ class AnnouncementsController < ApplicationController
   # this action lets us create a new announcement
   def create
     @announcement = @sport.announcements.new(announcement_params)
-    if @announcement.save
+    if @announcement.save && current_user.admin?
       render_success 200, true, 'announcement created successfully', @announcement.as_json
     else
-      if announcement.errors
-        errors = announcement.errors.full_messages.join(", ")
+      if @announcement.errors
+        errors = @announcement.errors.full_messages.join(", ")
       else
         errors = 'announcement creation failed'
       end
-      return_error 500, false, errors, {}
+      return_error 500, false, 'only admin can create announcements'
     end
   end
 
   # Update announcement API
   def update
-    if @announcement.update(announcement_params)
+    if @announcement.update(announcement_params) && current_user.admin?
       render_success 200, true, 'announcement updated successfully', @announcement.as_json
     else
       if @announcement.errors
@@ -34,7 +34,7 @@ class AnnouncementsController < ApplicationController
       else
         errors = 'announcement update failed'
       end
-      return_error 500, false, errors, {}
+      return_error 500, false, 'only admin can update announcements'
     end
   end
 
@@ -45,8 +45,11 @@ class AnnouncementsController < ApplicationController
 
   # Delete an announcement API
   def destroy
-    @announcement.destroy
-    render_success 200, true, 'announcement deleted successfully', {}
+    if @announcement.destroy && current_user.admin?
+      render_success 200, true, 'announcement deleted successfully', {}
+    else
+      return_error 500, false, 'only admin can delete announcement'
+    end
   end
   private
   def set_sport
