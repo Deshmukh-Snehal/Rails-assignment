bdecf73f9499cb59b4869b16489efa09894725e1
Deshmukh-Snehal <deshmukhs344@gmail.com>
2022-02-22 17:28:37 +0530

    add schema info in model and add validations for achievement module

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,3 +1,12 @@
 class Achievement < ApplicationRecord
+  #Achievement schema
+  # t.string "award"
+  # t.string "medal"
+  # t.integer "user_id"
+
+  #Association
   belongs_to :user
+
+  #Validations
+  validates :award, :medal, presence: true
 end
