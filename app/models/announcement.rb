6f197970bd880bf2a7e0e23ba6e3cc67d9f2915c
Deshmukh-Snehal <deshmukhs344@gmail.com>
2022-02-22 17:25:49 +0530

    add schema info in model and add validations for announcement module

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,12 @@
 class Announcement < ApplicationRecord
+  #Announcement schema
+  # t.string "title"
+  # t.text "description"
+  # t.integer "sport_id"
+
+  #Association
   belongs_to :sport
+
+  #Validations
+  validates :title, :description, presence: true
 end
