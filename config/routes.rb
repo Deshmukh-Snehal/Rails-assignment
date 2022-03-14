42181b1600b23f51d7ab2fd7fbe2f741f50bec68
Deshmukh-Snehal <deshmukhs344@gmail.com>
2022-03-14 15:16:30 +0530

    adding offers routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
   
   resources :businesses
   get '/fetch_businesses', to: 'businesses#fetch_businesses'
+
+  resources :offers do
+    collection do
+      get '/generate_offer_json', to: 'offers#generate_offer_json', as: :generate_offer_json
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
